a6b7ae54ed2a4fdd9ea3d4ec96e7fbec8ef72283
Conor Flynn <conor@glimpse.ie>
2021-03-14 13:36:28 +0000

    Bumps to 0.2.0.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ INSTALL_REQUIRES = [
 setup(
     name='spotibar',
     description='Spotify plugin for Polybar',
-    version='0.1.8',
+    version='0.2.0',
     url='https://github.com/conor-f/spotibar',
     python_requires='>=3.6',
     packages=find_packages('src'),
