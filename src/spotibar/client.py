d68a91d36b37cf24c1ba5ad51c0e7cc359a298a0
Conor Flynn <conor@glimpse.ie>
2022-03-18 12:10:41 +0000

    Fixes redirect URL for initial auth.

--- src/spotibar/client.py
+++ src/spotibar/client.py
@@ -27,7 +27,7 @@ class SpotibarClient():
             self.config.get('currently_playing_trunclen', 45)
         )
 
-        self.redirect_uri = "http://127.0.0.1:8080"
+        self.redirect_uri = "http://127.0.0.1"
         self.cache_dir = os.path.expanduser("~") + "/.spotibar_cache"
 
         if not os.path.exists(self.cache_dir):
